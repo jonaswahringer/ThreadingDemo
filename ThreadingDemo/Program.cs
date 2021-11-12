79d098fb588ec8c535a82092c51e37dd6f6d7730
Jonas Wahringer <jonas.wahringer@gmail.com>
2021-11-12 11:02:43 +0100

    first commit

---
+++ ThreadingDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ThreadingDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
