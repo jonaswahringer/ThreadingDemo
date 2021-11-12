8a51d96f58922c51f0f0d269765aaef46b2fc326
Jonas Wahringer <jonas.wahringer@gmail.com>
2021-11-12 11:47:16 +0100

    finished demo

---
+++ ThreadingDemo/PanelProducer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadingDemo
+{
+    public class PanelProducer
+    {
+        private static int TotalPanelAmount;
+        private static object totalAmountLock = new object(); //statisches lock object für lock
+
+        public int PanelAmount { get; set; }
+        public bool ShouldStop { get; set; }
+
+        public void Run()
+        {
+            //lock (this)
+            //{
+            for (int i = 0; i < PanelAmount && !ShouldStop; i++)
+            {
+                Console.WriteLine($"Panel {i} produced.");
+                // gemeinsam genutzte Resource
+                lock(totalAmountLock) //Mutual exclusion für statisches lock object
+                {
+                    TotalPanelAmount++;
+                    Console.WriteLine($"Total Amount of {TotalPanelAmount} produced.");
+                }
+                Thread.Sleep(1500);
+            }
+            //}
+        }
+    }
+}
--- ThreadingDemo/Program.cs
+++ ThreadingDemo/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ThreadingDemo
 {
@@ -6,7 +8,32 @@ namespace ThreadingDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            PanelProducer producer1 = new PanelProducer {PanelAmount=5};
+            Thread thread1 = new Thread(new ThreadStart(producer1.Run)); //Methoden Verweis -> ohne Klammern
+            thread1.Start();
+
+            // thread1.Join(); //wartet bis Thread1 fertig ist -> dann wird Thread2 gestartet
+
+            PanelProducer producer2 = new PanelProducer {PanelAmount = 10};
+            Thread thread2 = new Thread(new ThreadStart(producer2.Run)); //Methoden Verweis -> ohne Klammern "Wenn Thread gestartet, soll diese Methode aufgerufen werden"
+            thread2.Start();
+
+            Thread.Sleep(2000);
+            producer2.ShouldStop = true;
+
+            Task.Run(() =>
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine($"Anonymer Producer: Panel {i} produced.");
+                    Thread.Sleep(500);
+                    // if (i == 2)
+                    //    throw new Exception("Fehler im Producer");
+                }
+            });
+
+            Task.Run(() => new PanelProducer { PanelAmount = 3 }.Run());
+
         }
     }
 }
